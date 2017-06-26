56ae01bdb39d8b2e51c9d32d15f317ce87eb62df
ethangmt <ethang@live.com.mx>
2017-06-25 20:56:21 -0500

    paddles, ball collisions

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager Instance = null;
+
+	public AudioClip hit;
+
+	private AudioSource audioEffects;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy (gameObject);
+		}
+
+		AudioSource[] sources = GetComponents<AudioSource> ();
+
+		foreach (AudioSource source in sources) {
+			if (source.clip == null) {
+				audioEffects = source;
+			}
+		}
+	}
+
+	public void PlayOneShot (AudioClip audio) {
+		audioEffects.PlayOneShot (audio);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	public float speed = 30;
+
+	private Rigidbody2D rigidBody;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		rigidBody.velocity = Vector2.right * speed;
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		// wall top or bottom
+		if (collision.gameObject.name == "wall_top" || collision.gameObject.name == "wall_bottom") {
+			SoundManager.Instance.PlayOneShot (SoundManager.Instance.hit);
+		}
+
+		if (collision.gameObject.name == "leftPaddle" || collision.gameObject.name == "rightPaddle") {
+			handlePaddleHit (collision);
+		}
+
+		if (collision.gameObject.name == "wall_left" || collision.gameObject.name == "wall_right") {
+			SoundManager.Instance.PlayOneShot (SoundManager.Instance.hit);
+
+			//TODO update score
+
+			transform.position = new Vector2 (0, 0);
+		}
+	}
+
+	void handlePaddleHit (Collision2D coll) {
+		float y = ballHitPaddle (transform.position, coll.transform.position, coll.collider.bounds.size.y);
+
+		Vector2 dir = new Vector2 ();
+
+		if (coll.gameObject.name == "leftPaddle") {
+			dir = new Vector2 (1, y).normalized; 
+		}
+
+		if (coll.gameObject.name == "rightPaddle") {
+			dir = new Vector2 (-1, y).normalized; 
+		}
+
+		rigidBody.velocity = dir * speed;
+
+		SoundManager.Instance.PlayOneShot (SoundManager.Instance.hit);
+	}
+
+	float ballHitPaddle (Vector2 ball, Vector2 paddle, float paddleHeight) {
+		return (ball.y - paddle.y) / paddleHeight;
+	}
+}
